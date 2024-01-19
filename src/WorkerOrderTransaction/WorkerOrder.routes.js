c8fb74a9627136a334bff1c4d8d252f61e3c6448
unknown <vinothpukal6580@gmail.com>
2024-01-19 14:31:34 +0530

    message

--- src/WorkerOrderTransaction/WorkerOrder.routes.js
+++ src/WorkerOrderTransaction/WorkerOrder.routes.js
@@ -1,15 +1,17 @@
 const express = require("express");
-const { createWorkOrder, createImageTransaction, getAllWorkOrder, getAllWorkOrderTransaction } = require("./WorkerOrder.controllers");
+const { createWorkOrder, createImageTransaction, getAllWorkOrder, getAllWorkOrderTransaction,getWorkOrderTransaction } = require("./WorkerOrder.controllers");
 
 const router =express.Router()
 
 router.post('/workOrder',createWorkOrder);
 
-router.get('/workOrder',getAllWorkOrder);
+router.get('/workOrder',getWorkOrderTransaction);
 
 router.get('/workOrderTransaction',getAllWorkOrderTransaction);
 
 router.post('/image',createImageTransaction);
+
+
 
 
 module.exports =router
